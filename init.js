ea0b1bf54170f9e3ffde64917a666ea1591a3b31
ribensky <rn.merzius@cgodin.qc.ca>
2020-03-08 19:40:23 -0400

    init img gardes

--- init.js
+++ init.js
@@ -167,17 +167,176 @@ function initImages() {
     objImageGardeT = new Image();
     objImageGardeT.src = 'Personnages/GardeVert/New Piskel-41.png.png';
 
-    // image gardeV (vert)
-    objImageGardeV = new Image();
-    objImageGardeV.src = 'Personnages/GardeVert/sprite_dpl_droite_gardeVert.png';
+    // image gardeV dpl droite (vert)
+    objImageGardeVDroite = new Image();
+    objImageGardeVDroite.src = 'Personnages/GardeVert/sprite_dpl_droite_gardeVert.png';
 
-    // image gardeM (mauve)
-    objImageGardeM = new Image();
-    //objImageGardeM.src = 'Personnages/GardeVert/sprite_dpl_droite_gardeVert.png';
+    // image gardeV dpl droite (vert) avec lingot
+    objImageGardeVDroiteOr = new Image();
+    objImageGardeVDroiteOr.src = 'Personnages/GardeVert/sprite_dpl_droite_gardeVert_or.png';
+    
+    // image dplGauche garde vert
+    objImageGardeVGauche = new Image();
+    objImageGardeVGauche.src = 'Personnages/GardeVert/sprite_dpl_gauche_gardeVert.png';
+
+    // image dplGauche garde vert avec lingot 
+    objImageGardeVGaucheOr = new Image();
+    objImageGardeVGaucheOr.src = 'Personnages/GardeVert/sprite_dpl_gauche_gardeVert_or.png';
+
+    // image echelle garde vert
+    objImageGardeVEchelle = new Image();
+    objImageGardeVEchelle.src = 'Personnages/GardeVert/sprite_echelle_gardeVert.png';
+
+    // image echelle garde vert avec lingot
+    objImageGardeVEchelleOr = new Image();
+    objImageGardeVEchelleOr.src = 'Personnages/GardeVert/sprite_echelle_gardeVert_or.png';
+
+    // image barre gauche garde vert
+    objImageGardeVBarreGauche = new Image();
+    objImageGardeVBarreGauche.src = 'Personnages/GardeVert/sprite_barre_gauche_gardeVert.png';
+
+    // image barre gauche garde vert avec lingot
+    objImageGardeVBarreGaucheOr = new Image();
+    objImageGardeVBarreGaucheOr.src = 'Personnages/GardeVert/sprite_barre_gauche_gardeVert_or.png';
+
+     // image barre droite garde vert
+     objImageGardeVBarreDroite = new Image();
+     objImageGardeVBarreDroite.src = 'Personnages/GardeVert/sprite_barre_droite_gardeVert.png';
+
+     // image barre droite garde vert avec lingot
+     objImageGardeVBarreDroiteOr = new Image();
+     objImageGardeVBarreDroiteOr.src = 'Personnages/GardeVert/sprite_barre_gauche_gardeVert_or.png';
+
+     // image chute garde vert 
+     objImageGardeVChute = new Image();
+     objImageGardeVChute.src = 'Personnages/GardeVert/sprite_chute_gardeVert.png';
+
+     // image chute garde vert avec lingot
+     objImageGardeVChuteOr = new Image();
+     objImageGardeVChuteOr.src = 'Personnages/GardeVert/sprite_chute_gardeVert_or.png';
+
+     // image chute garde vert dans un trou
+     objImageGardeVChuteTrou = new Image();
+     objImageGardeVChuteTrou.src = 'Personnages/GardeVert/chute_gardeVert.png';
+
+     // image chute garde vert dans un trou avec lingot
+     objImageGardeVChuteTrouOr = new Image();
+     objImageGardeVChuteTrouOr.src = 'Personnages/GardeVert/chute_gardeVert_or.png';
+
+
+
+    // image gardeV dpl droite (Mauve)
+    objImageGardeMDroite = new Image();
+    objImageGardeMDroite.src = 'Personnages/GardeMauve/sprite_dpl_droite_gardeMauve.png';
+
+    // image gardeM dpl droite (mauve) avec lingot
+    objImageGardeMDroiteOr = new Image();
+    objImageGardeMDroiteOr.src = 'Personnages/GardeMauve/sprite_dpl_droite_gardeMauve_or.png';
+    
+    // image dplGauche garde mauve
+    objImageGardeMGauche = new Image();
+    objImageGardeMGauche.src = 'Personnages/GardeMauve/sprite_dpl_gauche_gardeMauve.png';
+
+    // image dplGauche garde mauve avec lingot 
+    objImageGardeMGaucheOr = new Image();
+    objImageGardeMGaucheOr.src = 'Personnages/GardeMauve/sprite_dpl_gauche_gardeMauve_or.png';
+
+    // image echelle garde mauve
+    objImageGardeMEchelle = new Image();
+    objImageGardeMEchelle.src = 'Personnages/GardeMauve/sprite_echelle_gardeMauve.png';
+
+    // image echelle garde mauve avec lingot
+    objImageGardeMEchelleOr = new Image();
+    objImageGardeMEchelleOr.src = 'Personnages/GardeMauve/sprite_echelle_gardeMauve_or.png';
+
+    // image barre gauche garde mauve
+    objImageGardeMBarreGauche = new Image();
+    objImageGardeMBarreGauche.src = 'Personnages/GardeMauve/sprite_barre_gauche_gardeMauve.png';
+
+    // image barre gauche garde mauve avec lingot
+    objImageGardeMBarreGaucheOr = new Image();
+    objImageGardeMBarreGaucheOr.src = 'Personnages/GardeMauve/sprite_barre_gauche_gardeMauve_or.png';
+
+     // image barre droite garde mauve
+     objImageGardeMBarreDroite = new Image();
+     objImageGardeMBarreDroite.src = 'Personnages/GardeMauve/sprite_barre_droite_gardeMauve.png';
+
+     // image barre droite garde mauve avec lingot
+     objImageGardeMBarreDroiteOr = new Image();
+     objImageGardeMBarreDroiteOr.src = 'Personnages/GardeMauve/sprite_barre_droite_gardeMauve_or.png';
+
+     // image chute garde mauve 
+     objImageGardeMChute = new Image();
+     objImageGardeMChute.src = 'Personnages/GardeMauve/sprite_chute_gardeMauve.png';
+
+     // image chute garde mauve avec lingot
+     objImageGardeMChuteOr = new Image();
+     objImageGardeMChuteOr.src = 'Personnages/GardeMauve/sprite_chute_gardeMauve_or.png';
+
+     // image chute garde mauve dans un trou
+     objImageGardeMChuteTrou = new Image();
+     objImageGardeMChuteTrou.src = 'Personnages/GardeMauve/chute_gardeMauve.png';
+
+     // image chute garde mauve dans un trou avec lingot
+     objImageGardeMChuteTrouOr = new Image();
+     objImageGardeMChuteTrouOr.src = 'Personnages/GardeMauve/chute_gardeMauve_or.png';
         
-    // image gardeR (rouge)
-    objImageGardeR = new Image();
-    //objImageGardeR.src = 'Personnages/GardeVert/sprite_dpl_droite_gardeVert.png';
+    
+     /// image gardeR dpl droite (rouge)
+    objImageGardeRDroite = new Image();
+    objImageGardeRDroite.src = 'Personnages/GardeRouge/sprite_dpl_droite_gardeRouge.png';
+
+    // image gardeR dpl droite (rouge) avec lingot
+    objImageGardeRDroiteOr = new Image();
+    objImageGardeRDroiteOr.src = 'Personnages/GardeRouge/sprite_dpl_droite_gardeRouge_or.png';
+    
+    // image dplGauche garde rouge
+    objImageGardeRGauche = new Image();
+    objImageGardeRGauche.src = 'Personnages/GardeRouge/sprite_dpl_gauche_gardeRouge.png';
+
+    // image dplGauche garde rouge avec lingot 
+    objImageGardeRGaucheOr = new Image();
+    objImageGardeRGaucheOr.src = 'Personnages/GardeRouge/sprite_dpl_gauche_gardeRouge_or.png';
+
+    // image echelle garde rouge
+    objImageGardeREchelle = new Image();
+    objImageGardeREchelle.src = 'Personnages/GardeRouge/sprite_echelle_gardeRouge.png';
+
+    // image echelle garde rouge avec lingot
+    objImageGardeREchelleOr = new Image();
+    objImageGardeREchelleOr.src = 'Personnages/GardeRouge/sprite_echelle_gardeRouge_or.png';
+
+    // image barre gauche garde rouge
+    objImageGardeRBarreGauche = new Image();
+    objImageGardeRBarreGauche.src = 'Personnages/GardeRouge/sprite_barre_gauche_gardeRouge.png';
+
+    // image barre gauche garde rouge avec lingot
+    objImageGardeRBarreGaucheOr = new Image();
+    objImageGardeRBarreGaucheOr.src = 'Personnages/GardeRouge/sprite_barre_gauche_gardeRouge_or.png';
+
+     // image barre droite garde rouge
+     objImageGardeRBarreDroite = new Image();
+     objImageGardeRBarreDroite.src = 'Personnages/GardeRouge/sprite_barre_droite_gardeRouge.png';
+
+     // image barre droite garde rouge avec lingot
+     objImageGardeRBarreDroiteOr = new Image();
+     objImageGardeRBarreDroiteOr.src = 'Personnages/GardeRouge/sprite_barre_droite_gardeRouge_or.png';
+
+     // image chute garde rouge 
+     objImageGardeRChute = new Image();
+     objImageGardeRChute.src = 'Personnages/GardeRouge/sprite_chute_gardeRouge.png';
+
+     // image chute garde rouge avec lingot
+     objImageGardeRChuteOr = new Image();
+     objImageGardeRChuteOr.src = 'Personnages/GardeRouge/sprite_chute_gardeRouge_or.png';
+
+     // image chute garde rouge dans un trou
+     objImageGardeRChuteTrou = new Image();
+     objImageGardeRChuteTrou.src = 'Personnages/GardeRouge/chute_gardeRouge.png';
+
+     // image chute garde rouge dans un trou avec lingot
+     objImageGardeRChuteTrouOr = new Image();
+     objImageGardeRChuteTrouOr.src = 'Personnages/GardeRouge/chute_gardeRouge_or.png';
 
     // image echelle
     objLodeEchelle = new Image();
@@ -187,10 +346,11 @@ function initImages() {
     objLodeChute = new Image();
     objLodeChute.src = "Personnages/LodeRunner/sprite_chute.png";
 
-    // image chute sans frames
+    // image chute Lode sans frames (dans un trou)
     objLodeChuteSolo = new Image();
     objLodeChuteSolo.src = "Personnages/LodeRunner/loderunner_chute_1.png";
     
+
 
     // image barre droite 
     objLodeBarreDroite = new Image();
